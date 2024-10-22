b8752b8ff5f1fb666b417579c627e9d6a4a98afb
yosuke-oka <y.oka711@gmail.com>
2024-10-22 12:14:19 +0900

    Add test cases for UpdateUser and DeleteUser functions

--- internal/client/user_test.go
+++ internal/client/user_test.go
@@ -181,11 +181,11 @@ func TestCreateUser(t *testing.T) {
 				"email": "test@example.com",
 				"role": "admin",
 				"can_use_audit_log": true,
-				"is_restricted_connection_modify": false
+				"is_restricted_connection_modify": false,
 				"last_sign_in_at": "2024-07-29T19:00:00.000+09:00",
 				"created_at": "2024-07-29T19:00:00.000+09:00",
 				"updated_at": "2024-07-29T20:00:00.000+09:00"
-			}
+    		}
 		`
 		_, err := w.Write([]byte(resp))
 		if err != nil {
@@ -207,7 +207,7 @@ func TestCreateUser(t *testing.T) {
 	}
 	cases := []Case{
 		{"ID", output.ID, int64(1)},
-		{"email", output.Email, "test@exmaple.com"},
+		{"email", output.Email, "test@example.com"},
 		{"role", output.Role, "admin"},
 		{"can_use_audit_log", output.CanUseAuditLog, true},
 		{"is_restricted_connection_modify", output.IsRestrictedConnectionModify, false},
@@ -216,4 +216,79 @@ func TestCreateUser(t *testing.T) {
 		{"updated_at", output.UpdatedAt, "2024-07-29T20:00:00.000+09:00"},
 	}
 	testCases(t, cases)
+}
+
+// UpdateUser
+
+func TestUpdateUser(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cases := []Case{
+			{"path", r.URL.Path, "/api/users/1"},
+			{"method", r.Method, http.MethodPatch},
+		}
+		testCases(t, cases)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		resp := `
+			{
+				"id": 1,
+				"email": "test@example.com",
+				"role": "admin",
+				"can_use_audit_log": true,
+				"is_restricted_connection_modify": false,
+				"last_sign_in_at": "2024-07-29T19:00:00.000+09:00",
+				"created_at": "2024-07-29T19:00:00.000+09:00",
+				"updated_at": "2024-07-29T20:00:00.000+09:00"
+    		}
+		`
+		_, err := w.Write([]byte(resp))
+		if err != nil {
+			t.Errorf("Expected no error, got %s", err)
+		}
+	}))
+	defer server.Close()
+
+	client := NewDevTroccoClient("1234567890", server.URL)
+	input := UpdateUserInput{
+		Role:                         "admin",
+		CanUseAuditLog:               true,
+		IsRestrictedConnectionModify: false,
+	}
+	output, err := client.UpdateUser(1, &input)
+	if err != nil {
+		t.Errorf("Expected no error, got %s", err)
+	}
+	cases := []Case{
+		{"ID", output.ID, int64(1)},
+		{"email", output.Email, "test@example.com"},
+		{"role", output.Role, "admin"},
+		{"can_use_audit_log", output.CanUseAuditLog, true},
+		{"is_restricted_connection_modify", output.IsRestrictedConnectionModify, false},
+		{"last_sign_in_at", output.LastSignInAt, "2024-07-29T19:00:00.000+09:00"},
+		{"created_at", output.CreatedAt, "2024-07-29T19:00:00.000+09:00"},
+		{"updated_at", output.UpdatedAt, "2024-07-29T20:00:00.000+09:00"},
+	}
+	testCases(t, cases)
+
+}
+
+// DeleteUser
+
+func TestDeleteUser(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cases := []Case{
+			{"path", r.URL.Path, "/api/users/1"},
+			{"method", r.Method, http.MethodDelete},
+		}
+		testCases(t, cases)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNoContent)
+	}))
+	defer server.Close()
+
+	client := NewDevTroccoClient("1234567890", server.URL)
+	err := client.DeleteUser(1)
+	if err != nil {
+		t.Errorf("Expected no error, got %s", err)
+	}
 }
