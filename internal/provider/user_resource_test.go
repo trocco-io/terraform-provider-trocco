4a11c8e1ae5eb8fab83193443b2debd46a8459db
yosuke-oka <y.oka711@gmail.com>
2024-10-29 15:54:28 +0900

    add user_resouce acceptance test

--- internal/provider/user_resource_test.go
+++ internal/provider/user_resource_test.go
@@ -1,6 +1,7 @@
 package provider
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -20,18 +21,120 @@ func TestAccUserResource(t *testing.T) {
 					}
 				`,
 				Check: resource.ComposeAggregateTestCheckFunc(
-
 					resource.TestCheckResourceAttr("trocco_user.test", "email", "test@example.com"),
+					resource.TestCheckResourceAttr("trocco_user.test", "role", "admin"),
+					resource.TestCheckResourceAttr("trocco_user.test", "can_use_audit_log", "false"),
+					resource.TestCheckResourceAttr("trocco_user.test", "is_restricted_connection_modify", "false"),
+					resource.TestCheckResourceAttrSet("trocco_user.test", "id"),
 				),
 			},
 			// ImportState testing
 			{
-				ResourceName:      "trocco_user.test",
-				ImportState:       true,
-				ImportStateVerify: true,
-				//ImportStateVerifyIgnore: []string{"last_updated"},
+				ResourceName:            "trocco_user.test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"password"},
 			},
 			// Update and Read testing
+			{
+				Config: providerConfig + `
+					resource "trocco_user" "test" {
+					  email = "test@example.com"
+					  role = "member"
+					  can_use_audit_log = true
+					  is_restricted_connection_modify = true
+					}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("trocco_user.test", "email", "test@example.com"),
+					resource.TestCheckResourceAttr("trocco_user.test", "role", "member"),
+					resource.TestCheckResourceAttr("trocco_user.test", "can_use_audit_log", "true"),
+					resource.TestCheckResourceAttr("trocco_user.test", "is_restricted_connection_modify", "true"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccUserResourceInvalidEmail(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + `
+					resource "trocco_user" "test" {
+					  email = "test"
+					  password = "3XRambMkp-Hw"
+					  role = "admin"
+					}
+				`,
+				ExpectError: regexp.MustCompile(`invalid email address`),
+			},
+		},
+	})
+}
+
+func TestAccUserResourceInvalidPassword(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + `
+					resource "trocco_user" "test" {
+					  email = "test@example.com"
+					  password = "abc123"
+					  role = "admin"
+					}
+				`,
+				ExpectError: regexp.MustCompile(`password string length`),
+			},
+			{
+				Config: providerConfig + `
+					resource "trocco_user" "test" {
+					  email = "test@example.com"
+					  password = "1111111111111"
+					  role = "admin"
+					}
+				`,
+				ExpectError: regexp.MustCompile(`must contain at least one letter`),
+			},
+			{
+				Config: providerConfig + `
+					resource "trocco_user" "test" {
+					  email = "test@example.com"
+					  password = "aaaaaaaaaaaaa"
+					  role = "admin"
+					}
+				`,
+				ExpectError: regexp.MustCompile(`must contain at least one number`),
+			},
+			{
+				Config: providerConfig + `
+					resource "trocco_user" "test" {
+					  email = "test@example.com"
+					  role = "admin"
+					}
+				`,
+				ExpectError: regexp.MustCompile(`Missing Required Attribute`),
+			},
+		},
+	})
+}
+
+func TestAccUserResourceInvalidRole(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + `
+					resource "trocco_user" "test" {
+					  email = "test@example.com"
+					  password = "abc123"
+					  role = "invalid role"
+					}
+				`,
+				ExpectError: regexp.MustCompile(`role value must be one of`),
+			},
 		},
 	})
 }
