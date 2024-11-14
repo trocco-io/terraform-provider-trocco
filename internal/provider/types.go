ca0c533e914e2628906d7da61cfd61b9d616fbee
yosuke-oka <y.oka711@gmail.com>
2024-11-14 15:13:11 +0900

    Fix provider/types.go

--- internal/provider/types.go
+++ internal/provider/types.go
@@ -6,15 +6,12 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
+// NewNullableFromTerraformString create a client.NullableString from a types.String.
+func newNullableFromTerraformString(v types.String) *client.NullableString {
+	return &client.NullableString{Valid: !v.IsNull(), Value: v.ValueString()}
+}
+
 // NewNullableFromTerraformBool create a client.NullableBool from a types.Bool.
 func newNullableFromTerraformBool(v types.Bool) *client.NullableBool {
 	return &client.NullableBool{Valid: !v.IsNull(), Value: v.ValueBool()}
-}
-
-func ExampleNewNullableFromTerraformBool() {
-	newNullableFromTerraformBool(types.BoolNull())
-	// Output: b1.Valid = false
-
-	newNullableFromTerraformBool(types.BoolValue(true))
-	// Output: b2.Valid = true, b2.Value = true
 }
