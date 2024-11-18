d6ec15d49da8644a444ebce65226a9afd336d6dc
okeyaki <shinya.otsuki.4d@gmail.com>
2024-11-18 15:18:14 +0900

    fix example

--- examples/resources/connection/bigquery.tf
+++ examples/resources/connection/bigquery.tf
@@ -4,6 +4,6 @@ resource "trocco_connection" "bigquery" {
   name        = "BigQuery Example"
   description = "This is a BigQuery connection example"
 
-  project_id = "example"
+  project_id               = "example"
   service_account_json_key = "{\"type\":\"service_account\", ...}"
 }
