5bab26b981db12614f9e16be09a265341662b9b5
yosuke-oka <y.oka711@gmail.com>
2024-12-13 17:51:00 +0900

    Add team resource interface

--- internal/provider/team_resource.go
+++ internal/provider/team_resource.go
@@ -1,1 +1,58 @@
 package provider
+
+import (
+	"context"
+	"terraform-provider-trocco/internal/client"
+
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var (
+	_ resource.Resource                = &teamResource{}
+	_ resource.ResourceWithConfigure   = &teamResource{}
+	_ resource.ResourceWithImportState = &teamResource{}
+)
+
+func NewTeamResource() resource.Resource {
+	return &teamResource{}
+}
+
+type teamResource struct {
+	client *client.TroccoClient
+}
+
+type teamResourceModel struct {
+	ID          types.Int64               `tfsdk:"id"`
+	Name        types.String              `tfsdk:"name"`
+	Description types.String              `tfsdk:"description"`
+	Members     []teamMemberResourceModel `tfsdk:"members"`
+}
+
+type teamMemberResourceModel struct {
+	UserID types.Int64  `tfsdk:"user_id"`
+	Role   types.String `tfsdk:"role"`
+}
+
+func (r *teamResource) Metadata(ctx context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_team"
+}
+
+func (r *teamResource) Configure(ctx context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+}
+
+func (r *teamResource) Schema(ctx context.Context, req resource.SchemaRequest, resp *resource.SchemaResponse) {
+}
+
+func (r *teamResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+}
+
+func (r *teamResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+}
+func (r *teamResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+}
+func (r *teamResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+}
+
+func (r *teamResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+}
