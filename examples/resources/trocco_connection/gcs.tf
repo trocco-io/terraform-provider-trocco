6b1ca521278b32b602c29a583f0d01a636723e77
u110 <web.u.project@gmail.com>
2025-01-09 11:45:42 +0900

    add example

---
+++ examples/resources/trocco_connection/gcs.tf
@@ -0,0 +1,17 @@
+resource "trocco_connection" "gcs" {
+  connection_type = "gcs"
+  name        = "GCS Example"
+  description = "This is a Google Cloud Storage(GCS) connection example"
+
+  service_account_json_key = <<JSON
+  {
+    "type": "service_account",
+    "project_id": "example-project-id",
+    "private_key_id": "example-private-key-id",
+    "private_key":"-----BEGIN PRIVATE KEY-----\n..."
+  }
+  JSON
+  service_account_email = "joe@example-project.iam.gserviceaccount.com"
+  project_id  = "example-project-id"
+  application_name = "example-application-name"
+}
