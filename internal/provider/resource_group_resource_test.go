35ac22eba1778b71de0044bbf93c8c480ebeb7f1
Yuu Ito <u110@users.noreply.github.com>
2025-02-03 13:30:11 +0900

    Resource group (#36)
    * resource_group
    
    * rename
    
    * add docs using go generate
    
    * fix
    
    * add tests
    
    * golangci-lint
    
    * Update internal/client/resource_group.go
    
    * fix typo
    
    * undo move struct definition
    
    * use stringdefault
    
    * add length validator & misc

---
+++ internal/provider/resource_group_resource_test.go
@@ -0,0 +1,117 @@
+package provider
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccResourceGroupResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: providerConfig + `
+					resource "trocco_resource_group" "test" {
+					  name = "test"
+					  description = "test"
+					  teams = [
+						{
+						  team_id = 1
+						  role = "operator"
+						}
+					  ]
+					}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "name", "test"),
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "description", "test"),
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "teams.#", "1"),
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "teams.0.team_id", "1"),
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "teams.0.role", "operator"),
+				),
+			},
+			// ImportState testing
+			{
+				ResourceName:            "trocco_resource_group.test",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"id"},
+			},
+			// Update testing
+			{
+				Config: providerConfig + `
+					resource "trocco_resource_group" "test" {
+					  name = "updated"
+					  description = "updated"
+					  teams = [
+						{
+						  team_id = 1
+						  role = "administrator"
+						},
+						{
+						  team_id = 2
+						  role = "operator"
+						}
+					  ]
+					}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "name", "updated"),
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "description", "updated"),
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "teams.#", "2"),
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "teams.0.team_id", "1"),
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "teams.0.role", "administrator"),
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "teams.1.team_id", "2"),
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "teams.1.role", "operator"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceGroupNoTeams(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + `
+					resource "trocco_resource_group" "test" {
+					  name = "test"
+					  description = "test"
+					  teams = []
+					}
+				`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "name", "test"),
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "description", "test"),
+					resource.TestCheckResourceAttr("trocco_resource_group.test", "teams.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccResourceGroupDuplicateRoles(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: providerConfig + `
+					resource "trocco_resource_group" "test" {
+					  name = "test"
+					  description = "test"
+					  teams = [
+					    { team_id = 1, role = "administrator" },
+					    { team_id = 1, role = "operator" },
+					  ]
+					}
+				`,
+				ExpectError: regexp.MustCompile(`Team ID "1" is duplicated in the list.`),
+			},
+		},
+	})
+
+}
---
+++ internal/provider/validator/unique_team_validator.go
@@ -0,0 +1,46 @@
+package validator
+
+import (
+	"context"
+	"fmt"
+
+	model "terraform-provider-trocco/internal/provider/model/resource_group"
+
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+)
+
+type UniqueTeamValidator struct{}
+
+func (v UniqueTeamValidator) Description(ctx context.Context) string {
+	return "Ensures that team IDs are unique within the set."
+}
+
+func (v UniqueTeamValidator) MarkdownDescription(ctx context.Context) string {
+	return v.Description(ctx)
+}
+
+func (v UniqueTeamValidator) ValidateSet(ctx context.Context, request validator.SetRequest, response *validator.SetResponse) {
+	var teamIDs []model.TeamRoleResourceModel
+	diags := request.ConfigValue.ElementsAs(ctx, &teamIDs, false)
+	if diags.HasError() {
+		response.Diagnostics.Append(diags...)
+		return
+	}
+
+	for i, teamI := range teamIDs {
+		for j, teamJ := range teamIDs {
+			if i == j {
+				continue
+			}
+
+			if teamI.TeamID == teamJ.TeamID {
+				response.Diagnostics.AddAttributeError(
+					request.Path,
+					"Duplicate Team ID",
+					fmt.Sprintf("Team ID %q is duplicated in the list.", teamI.TeamID),
+				)
+				return
+			}
+		}
+	}
+}
