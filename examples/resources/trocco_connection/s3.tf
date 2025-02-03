3072248f629967b59f6b6e44802f592a419a6355
u110 <web.u.project@gmail.com>
2025-02-03 21:46:45 +0900

    fix example

--- examples/resources/trocco_connection/s3.tf
+++ examples/resources/trocco_connection/s3.tf
@@ -6,8 +6,8 @@ resource "trocco_connection" "s3" {
 
   aws_auth_type = "iam_user"
   aws_iam_user = {
-    access_key_id     = "AKIAIOSFODNN7EXAMPLE"
-    secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+    access_key_id     = "YOUR_ACCESS_KEY_ID"
+    secret_access_key = "YOUR_SECRET_ACCESS_KEY"
   }
 }
 
@@ -20,6 +20,6 @@ resource "trocco_connection" "s3_with_assume_role" {
   aws_auth_type = "assume_role"
   aws_assume_role = {
     account_id = "123456789012"
-    role_name  = "example-role"
+    role_name  = "YOUR_ROLE_NAME"
   }
 }
