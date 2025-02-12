e90ce3fec46e1d2e5449e1b98c24b1ff8fc09c39
mizuho.otake <otake@torcheees.com>
2025-02-12 04:11:25 +0000

    add postgresql.tf

---
+++ examples/resources/trocco_connection/postgresql.tf
@@ -0,0 +1,41 @@
+resource "trocco_connection" "postgresql" {
+  connection_type = "postgresql"
+  name            = "PostgreSQL Example"
+  description     = "This is a PostgreSQL connection example"
+  host            = "db.example.com"
+  port            = 5432
+  user_name       = "root"
+  password        = "password"
+  ssl_mode        = "required"
+  driver          = "postgresql_42_5_1"
+  ssl = {
+    ca   = <<-SSL_CA
+      -----BEGIN PRIVATE KEY-----
+      ...SSL CA...
+      -----END PRIVATE KEY-----
+    SSL_CA
+    cert = <<-SSL_CERT
+      -----BEGIN CERTIFICATE-----
+      ...SSL CRT...
+      -----END CERTIFICATE-----
+    SSL_CERT
+    key  = <<-SSL_KEY
+      -----BEGIN PRIVATE KEY-----
+      ...SSL KEY...
+      -----END PRIVATE KEY-----
+    SSL_KEY
+  }
+  gateway = {
+    host           = "gateway.example.com"
+    port           = 1234
+    user_name      = "gateway-joe"
+    password       = "gateway-joepass"
+    key            = <<-GATEWAY_KEY
+      -----BEGIN PRIVATE KEY-----
+      ... GATEWAY KEY...
+      -----END PRIVATE KEY-----
+    GATEWAY_KEY
+    key_passphrase = "sample_passphrase"
+  }
+  resource_group_id = 1
+}
