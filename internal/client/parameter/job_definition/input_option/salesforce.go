eb4bf45643896547c6d1406439b15f36fb7f3b07
kkatamot <kosuke.katamoto@primenumber.co.jp>
2025-02-19 11:17:22 +0900

    support salesforce input and output option (#54)
    * support salesforce output option
    
    * support output salesforce
    
    * add doc
    
    * fix lint
    
    * add example
    
    * refactor
    
    ---------
    
    Co-authored-by: Yuu Ito <u110@users.noreply.github.com>

---
+++ internal/client/parameter/job_definition/input_option/salesforce.go
@@ -0,0 +1,35 @@
+package input_options
+
+import (
+	"terraform-provider-trocco/internal/client/parameter"
+)
+
+type SalesforceInputOptionInput struct {
+	Object                          string                                  `json:"object"`
+	ObjectAcquisitionMethod         *parameter.NullableString               `json:"object_acquisition_method,omitempty"`
+	IsConvertTypeCustomColumns      *parameter.NullableBool                 `json:"is_convert_type_custom_columns,omitempty"`
+	IncludeDeletedOrArchivedRecords *parameter.NullableBool                 `json:"include_deleted_or_archived_records,omitempty"`
+	ApiVersion                      *parameter.NullableString               `json:"api_version,omitempty"`
+	Soql                            *parameter.NullableString               `json:"soql,omitempty"`
+	SalesforceConnectionID          int64                                   `json:"salesforce_connection_id"`
+	Columns                         []SalesforceColumn                      `json:"columns"`
+	CustomVariableSettings          *[]parameter.CustomVariableSettingInput `json:"custom_variable_settings,omitempty"`
+}
+
+type UpdateSalesforceInputOptionInput struct {
+	Object                          *string                                 `json:"object,omitempty"`
+	ObjectAcquisitionMethod         *parameter.NullableString               `json:"object_acquisition_method,omitempty"`
+	IsConvertTypeCustomColumns      *parameter.NullableBool                 `json:"is_convert_type_custom_columns,omitempty"`
+	IncludeDeletedOrArchivedRecords *parameter.NullableBool                 `json:"include_deleted_or_archived_records,omitempty"`
+	ApiVersion                      *parameter.NullableString               `json:"api_version,omitempty"`
+	Soql                            *parameter.NullableString               `json:"soql,omitempty"`
+	SalesforceConnectionID          *int64                                  `json:"salesforce_connection_id,omitempty"`
+	Columns                         []SalesforceColumn                      `json:"columns,omitempty"`
+	CustomVariableSettings          *[]parameter.CustomVariableSettingInput `json:"custom_variable_settings,omitempty"`
+}
+
+type SalesforceColumn struct {
+	Name   string  `json:"name"`
+	Type   string  `json:"type"`
+	Format *string `json:"format"`
+}
---
+++ internal/provider/planmodifier/salesforce_input_option_plan_modifier.go
@@ -0,0 +1,105 @@
+package planmodifier
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"strings"
+)
+
+var _ planmodifier.Object = &SalesforceInputOptionPlanModifier{}
+
+type SalesforceInputOptionPlanModifier struct{}
+
+func (d *SalesforceInputOptionPlanModifier) Description(ctx context.Context) string {
+	return "Modifier for validating salesforce input option attributes"
+}
+
+func (d *SalesforceInputOptionPlanModifier) MarkdownDescription(ctx context.Context) string {
+	return d.Description(ctx)
+}
+
+func (d *SalesforceInputOptionPlanModifier) PlanModifyObject(ctx context.Context, req planmodifier.ObjectRequest, resp *planmodifier.ObjectResponse) {
+	var soql types.String
+	resp.Diagnostics.Append(req.Plan.GetAttribute(ctx, req.Path.AtName("soql"), &soql)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var objectAcquisitionMethod types.String
+	resp.Diagnostics.Append(req.Plan.GetAttribute(ctx, req.Path.AtName("object_acquisition_method"), &objectAcquisitionMethod)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var isConvertTypeCustomColumns types.Bool
+	resp.Diagnostics.Append(req.Plan.GetAttribute(ctx, req.Path.AtName("is_convert_type_custom_columns"), &isConvertTypeCustomColumns)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var columns types.List
+	resp.Diagnostics.Append(req.Plan.GetAttribute(ctx, req.Path.AtName("columns"), &columns)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Verify that soql is not specified for all_columns
+	if objectAcquisitionMethod.ValueString() == "all_columns" && !soql.IsNull() {
+		addSalesforceInputOptionAttributeError(req, resp, "soql cannot be specified when object_acquisition_method is 'all_columns'")
+	}
+
+	// Verify that soql is specified for soql
+	if objectAcquisitionMethod.ValueString() == "soql" && soql.IsNull() {
+		addSalesforceInputOptionAttributeError(req, resp, "soql must be specified when object_acquisition_method is 'soql'")
+	}
+
+	// Verify that column type is valid
+	if isConvertTypeCustomColumns.ValueBool() {
+		for _, column := range columns.Elements() {
+			if columnObj, ok := column.(types.Object); ok {
+				attributes := columnObj.Attributes()
+				colType, ok := attributes["type"].(types.String)
+				if !ok {
+					resp.Diagnostics.AddError(
+						"Unexpected error",
+						"column type is not a string",
+					)
+				}
+				colName, ok := attributes["name"].(types.String)
+				if !ok {
+					resp.Diagnostics.AddError(
+						"Unexpected error",
+						"column name is not a string",
+					)
+				}
+				hasCustomSuffix := strings.HasSuffix(colName.ValueString(), "__c")
+				isNotString := colType.ValueString() != "string"
+				isNotBooleanOrString := colType.ValueString() != "boolean" && isNotString
+
+				if isNotBooleanOrString && hasCustomSuffix {
+					addSalesforceInputOptionAttributeError(req, resp, "column type must be 'boolean' or 'string' when is_convert_type_custom_columns is true and column name end with '__c'")
+				}
+				if isNotString && !hasCustomSuffix {
+					addSalesforceInputOptionAttributeError(req, resp, "column type must be 'string' when is_convert_type_custom_columns is true and column name does not end with '__c'")
+				}
+			} else {
+				resp.Diagnostics.AddError(
+					"Invalid Column Type",
+					"Expected column to be an object",
+				)
+				return
+			}
+		}
+	}
+
+}
+
+func addSalesforceInputOptionAttributeError(req planmodifier.ObjectRequest, resp *planmodifier.ObjectResponse, message string) {
+	resp.Diagnostics.AddAttributeError(
+		req.Path,
+		"SalesforceInputOption Validation Error",
+		fmt.Sprintf("Attribute %s %s", req.Path, message),
+	)
+}
