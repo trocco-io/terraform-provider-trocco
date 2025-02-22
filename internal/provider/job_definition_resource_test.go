04fd80e92ff7c08310cf397884480401788f9bc6
u110 <web.u.project@gmail.com>
2025-02-22 10:14:29 +0900

    wip: fix test for e2e

--- internal/provider/job_definition_resource_test.go
+++ internal/provider/job_definition_resource_test.go
@@ -14,20 +14,57 @@ func TestAccJobDefinitionResource(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config: providerConfig + `
-
-
+resource "trocco_connection" "test_mysql" {
+  connection_type = "mysql"
+  name            = "MySQL Example"
+  host            = "db.example.com"
+  port            = 65535
+  user_name       = "root"
+  password        = "password"
+}
+resource "trocco_connection" "test_bq" {
+  connection_type = "bigquery"
+  name        = "BigQuery Example"
+  project_id               = "example"
+  service_account_json_key = <<JSON
+  {
+    "type": "service_account",
+    "project_id": "example-project-id",
+    "private_key_id": "example-private-key-id",
+    "private_key":"-----BEGIN PRIVATE KEY-----\n..."
+  }
+  JSON
+}
+resource "trocco_team" "test" {
+  name        = "test"
+  members     = [
+    {
+      user_id = 10626
+      role    = "team_admin"
+    },
+  ]
+}
+resource "trocco_resource_group" "test" {
+  name        = "test"
+  description = "test"
+  teams     = [
+    {
+      team_id = trocco_team.test.id
+      role    = "administrator"
+    },
+  ]
+}
 resource "trocco_job_definition" "mysql_to_bigquery" {
   name                        = "test job_definition"
   description                 = "test description"
   resource_enhancement        = "large"
-  resource_group_id           = 1
+  resource_group_id           = trocco_resource_group.test.id
   retry_limit                 = 1
   is_runnable_concurrently    = true
   filter_columns              = [
     {
       default                      = ""
       format = "%Y"
-      json_expand_columns          = []
       json_expand_enabled          = false
       json_expand_keep_base_column = false
       name                         = "id"
@@ -36,7 +73,6 @@ resource "trocco_job_definition" "mysql_to_bigquery" {
     },
     {
       default                      = ""
-      json_expand_columns          = []
       json_expand_enabled          = false
       json_expand_keep_base_column = false
       name                         = "name"
@@ -46,7 +82,6 @@ resource "trocco_job_definition" "mysql_to_bigquery" {
     {
       default                      = ""
       format                       = ""
-      json_expand_columns          = []
       json_expand_enabled          = false
       json_expand_keep_base_column = false
       name                         = "created_at"
@@ -182,7 +217,7 @@ resource "trocco_job_definition" "mysql_to_bigquery" {
           type = "timestamp"
         },
       ]
-      mysql_connection_id         = 1
+      mysql_connection_id         = trocco_connection.test_mysql.id
       query                       = <<-EOT
                 select
                     *
@@ -204,7 +239,7 @@ resource "trocco_job_definition" "mysql_to_bigquery" {
       read_timeout_sec                           = 300
       send_timeout_sec                           = 300
       retries                                    = 2
-      bigquery_connection_id                     = 1
+      bigquery_connection_id                     = trocco_connection.test_bq.id
       location                                   = "us-west1"
       bigquery_output_option_clustering_fields   = []
       bigquery_output_option_merge_keys          = []
@@ -217,7 +252,6 @@ resource "trocco_job_definition" "mysql_to_bigquery" {
 					resource.TestCheckResourceAttr("trocco_job_definition.mysql_to_bigquery", "name", "test job_definition"),
 					resource.TestCheckResourceAttr("trocco_job_definition.mysql_to_bigquery", "description", "test description"),
 					resource.TestCheckResourceAttr("trocco_job_definition.mysql_to_bigquery", "resource_enhancement", "large"),
-					resource.TestCheckResourceAttr("trocco_job_definition.mysql_to_bigquery", "resource_group_id", "1"),
 					resource.TestCheckResourceAttr("trocco_job_definition.mysql_to_bigquery", "retry_limit", "1"),
 					resource.TestCheckResourceAttr("trocco_job_definition.mysql_to_bigquery", "is_runnable_concurrently", "true"),
 				),
