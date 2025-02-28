8e0446c28a0305c3dd2da9557ba4b1387ee54b7c
mizuho.otake <otake@torcheees.com>
2025-02-28 01:38:21 +0000

    fix review

--- internal/client/parameter/job_definition/input_option/postgresql.go
+++ internal/client/parameter/job_definition/input_option/postgresql.go
@@ -6,16 +6,16 @@ import (
 
 type PostgreSQLInputOptionInput struct {
 	Database                  string                                  `json:"database"`
-	Schema                    string                                  `json:"schema"`
+	Schema                    *parameter.NullableString               `json:"schema,omitempty"`
 	Table                     *parameter.NullableString               `json:"table,omitempty"`
 	Query                     *parameter.NullableString               `json:"query,omitempty"`
 	IncrementalColumns        *parameter.NullableString               `json:"incremental_columns,omitempty"`
 	LastRecord                *parameter.NullableString               `json:"last_record,omitempty"`
-	IncrementalLoadingEnabled bool                                    `json:"incremental_loading_enabled"`
-	FetchRows                 int64                                   `json:"fetch_rows"`
-	ConnectTimeout            int64                                   `json:"connect_timeout"`
-	SocketTimeout             int64                                   `json:"socket_timeout"`
-	DefaultTimeZone           string                                  `json:"default_time_zone"`
+	IncrementalLoadingEnabled *parameter.NullableBool                 `json:"incremental_loading_enabled,omitempty"`
+	FetchRows                 *parameter.NullableInt64                `json:"fetch_rows,omitempty"`
+	ConnectTimeout            *parameter.NullableInt64                `json:"connect_timeout,omitempty"`
+	SocketTimeout             *parameter.NullableInt64                `json:"socket_timeout,omitempty"`
+	DefaultTimeZone           *parameter.NullableString               `json:"default_time_zone,omitempty"`
 	PostgreSQLConnectionID    int64                                   `json:"postgresql_connection_id"`
 	InputOptionColumns        []PostgreSQLInputOptionColumn           `json:"input_option_columns"`
 	CustomVariableSettings    *[]parameter.CustomVariableSettingInput `json:"custom_variable_settings,omitempty"`
@@ -24,18 +24,18 @@ type PostgreSQLInputOptionInput struct {
 
 type UpdatePostgreSQLInputOptionInput struct {
 	Database                  *string                                 `json:"database,omitempty"`
-	Schema                    *string                                 `json:"schema,omitempty"`
+	Schema                    *parameter.NullableString               `json:"schema,omitempty"`
 	Table                     *parameter.NullableString               `json:"table,omitempty"`
 	Query                     *parameter.NullableString               `json:"query,omitempty"`
 	IncrementalColumns        *parameter.NullableString               `json:"incremental_columns,omitempty"`
 	LastRecord                *parameter.NullableString               `json:"last_record,omitempty"`
 	IncrementalLoadingEnabled *bool                                   `json:"incremental_loading_enabled,omitempty"`
-	FetchRows                 *int64                                  `json:"fetch_rows,omitempty"`
-	ConnectTimeout            *int64                                  `json:"connect_timeout,omitempty"`
-	SocketTimeout             *int64                                  `json:"socket_timeout,omitempty"`
-	DefaultTimeZone           *string                                 `json:"default_time_zone,omitempty"`
+	FetchRows                 *parameter.NullableInt64                `json:"fetch_rows,omitempty"`
+	ConnectTimeout            *parameter.NullableInt64                `json:"connect_timeout,omitempty"`
+	SocketTimeout             *parameter.NullableInt64                `json:"socket_timeout,omitempty"`
+	DefaultTimeZone           *parameter.NullableString               `json:"default_time_zone,omitempty"`
 	PostgreSQLConnectionID    *int64                                  `json:"postgresql_connection_id,omitempty"`
-	InputOptionColumns        []PostgreSQLInputOptionColumn           `json:"input_option_columns"`
+	InputOptionColumns        []PostgreSQLInputOptionColumn           `json:"input_option_columns,omitempty"`
 	CustomVariableSettings    *[]parameter.CustomVariableSettingInput `json:"custom_variable_settings,omitempty"`
 	InputOptionColumnOptions  *[]InputOptionColumnOptions             `json:"input_option_column_options,omitempty"`
 }
