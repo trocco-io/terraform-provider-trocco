36bf2d617d4032853f2dafb21016c2c74e727251
u110 <web.u.project@gmail.com>
2025-05-02 17:20:15 +0900

    add tests

---
+++ examples/testdata/job_definition/snowflake_to_bigquery/create.tf
@@ -0,0 +1,149 @@
+resource "trocco_connection" "test_snowflake" {
+  connection_type = "snowflake"
+  name            = "Snowflake Example"
+  description     = "This is a Snowflake connection example"
+  host            = "example.snowflakecomputing.com"
+  auth_method     = "user_password"
+  user_name       = "test_user"
+  password        = "test_password"
+}
+
+resource "trocco_connection" "test_bq" {
+  connection_type = "bigquery"
+  name            = "BigQuery Example"
+  project_id      = "example"
+  service_account_json_key = <<JSON
+  {
+    "type": "service_account",
+    "project_id": "example-project-id",
+    "private_key_id": "example-private-key-id",
+    "private_key":"-----BEGIN PRIVATE KEY-----\n..."
+  }
+  JSON
+}
+
+resource "trocco_team" "test" {
+  name    = "test"
+  members = [
+    {
+      user_id = 10626
+      role    = "team_admin"
+    },
+  ]
+}
+
+resource "trocco_resource_group" "test" {
+  name        = "test"
+  description = "test"
+  teams       = [
+    {
+      team_id = trocco_team.test.id
+      role    = "administrator"
+    },
+  ]
+}
+
+resource "trocco_job_definition" "snowflake_to_bigquery" {
+  name                     = "Snowflake to BigQuery Test"
+  description              = "Test job definition for transferring data from Snowflake to BigQuery"
+  resource_enhancement      = "medium"
+  resource_group_id        = trocco_resource_group.test.id
+  retry_limit              = 2
+  is_runnable_concurrently = true
+  
+  input_option_type = "snowflake"
+  input_option = {
+    snowflake_input_option = {
+      snowflake_connection_id = trocco_connection.test_snowflake.id
+      database                = "test_database"
+      schema                  = "PUBLIC"
+      warehouse               = "COMPUTE_WH"
+      query                   = "SELECT id, name, email, created_at FROM test_table"
+      input_option_columns = [
+        {
+          name = "id"
+          type = "long"
+        },
+        {
+          name = "name"
+          type = "string"
+        },
+        {
+          name = "email"
+          type = "string"
+        },
+        {
+          name = "created_at"
+          type = "timestamp"
+        }
+      ]
+    }
+  }
+  
+  filter_columns = [
+    {
+      default                      = null
+      json_expand_enabled          = false
+      json_expand_keep_base_column = false
+      name                         = "user_id"
+      src                          = "id"
+      type                         = "long"
+    },
+    {
+      default                      = null
+      json_expand_enabled          = false
+      json_expand_keep_base_column = false
+      name                         = "user_name"
+      src                          = "name"
+      type                         = "string"
+    },
+    {
+      default                      = null
+      json_expand_enabled          = false
+      json_expand_keep_base_column = false
+      name                         = "user_email"
+      src                          = "email"
+      type                         = "string"
+    },
+    {
+      default                      = null
+      format                       = "%Y-%m-%d"
+      json_expand_enabled          = false
+      json_expand_keep_base_column = false
+      name                         = "registration_date"
+      src                          = "created_at"
+      type                         = "timestamp"
+    }
+  ]
+  
+  output_option_type = "bigquery"
+  output_option = {
+    bigquery_output_option = {
+      dataset                                  = "test_dataset"
+      table                                    = "snowflake_to_bigquery_test_table"
+      mode                                     = "append"
+      auto_create_dataset                      = true
+      timeout_sec                              = 300
+      open_timeout_sec                         = 300
+      read_timeout_sec                         = 300
+      send_timeout_sec                         = 300
+      retries                                  = 3
+      bigquery_connection_id                   = trocco_connection.test_bq.id
+      location                                 = "US"
+      bigquery_output_option_clustering_fields = []
+      bigquery_output_option_column_options    = []
+      bigquery_output_option_merge_keys        = []
+    }
+  }
+  
+  # please create labels if testing in local environment
+  # see https://trocco.io/labels#side-nav-labels
+  # labels = [
+  #   {
+  #     name = "test_label1"
+  #   },
+  #   {
+  #     name = "test_label2"
+  #   },
+  # ]
+}
--- internal/provider/job_definition_resource_test.go
+++ internal/provider/job_definition_resource_test.go
@@ -374,3 +374,126 @@ func TestAccJobDefinitionResourceS3ToBigQuery(t *testing.T) {
 		},
 	})
 }
+
+func TestAccJobDefinitionResourceSalesforceToBigQuery(t *testing.T) {
+	resourceName := "trocco_job_definition.salesforce_to_bigquery"
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				ResourceName: resourceName,
+				Config:       providerConfig + LoadTextFile("../../examples/testdata/job_definition/salesforce_to_bigquery/create.tf"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", "Salesforce to BigQuery Test"),
+					resource.TestCheckResourceAttr(resourceName, "description", "Test job definition for transferring data from Salesforce to BigQuery"),
+					resource.TestCheckResourceAttr(resourceName, "resource_enhancement", "medium"),
+					resource.TestCheckResourceAttr(resourceName, "retry_limit", "2"),
+					resource.TestCheckResourceAttr(resourceName, "is_runnable_concurrently", "true"),
+					resource.TestCheckResourceAttr(resourceName, "input_option_type", "salesforce"),
+					resource.TestCheckResourceAttr(resourceName, "output_option_type", "bigquery"),
+					resource.TestCheckResourceAttr(resourceName, "input_option.salesforce_input_option.object", "Contact"),
+					resource.TestCheckResourceAttr(resourceName, "input_option.salesforce_input_option.object_acquisition_method", "soql"),
+					resource.TestCheckResourceAttr(resourceName, "input_option.salesforce_input_option.soql", "SELECT Id, Name, Email, CreatedDate FROM Contact"),
+					resource.TestCheckResourceAttr(resourceName, "filter_columns.0.name", "contact_id"),
+					resource.TestCheckResourceAttr(resourceName, "filter_columns.0.src", "Id"),
+					resource.TestCheckResourceAttr(resourceName, "filter_columns.0.type", "string"),
+					resource.TestCheckResourceAttr(resourceName, "output_option.bigquery_output_option.dataset", "test_dataset"),
+					resource.TestCheckResourceAttr(resourceName, "output_option.bigquery_output_option.table", "salesforce_to_bigquery_test_table"),
+					resource.TestCheckResourceAttr(resourceName, "output_option.bigquery_output_option.mode", "append"),
+				),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{},
+				ImportStateIdFunc: func(s *terraform.State) (string, error) {
+					jobDefinitionId := s.RootModule().Resources[resourceName].Primary.ID
+					return jobDefinitionId, nil
+				},
+			},
+		},
+	})
+}
+
+func TestAccJobDefinitionResourceBigQueryToSnowflake(t *testing.T) {
+	resourceName := "trocco_job_definition.bigquery_to_snowflake"
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				ResourceName: resourceName,
+				Config:       providerConfig + LoadTextFile("../../examples/testdata/job_definition/bigquery_to_snowflake/create.tf"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", "BigQuery to Snowflake Test"),
+					resource.TestCheckResourceAttr(resourceName, "description", "Test job definition for transferring data from BigQuery to Snowflake"),
+					resource.TestCheckResourceAttr(resourceName, "resource_enhancement", "medium"),
+					resource.TestCheckResourceAttr(resourceName, "retry_limit", "2"),
+					resource.TestCheckResourceAttr(resourceName, "is_runnable_concurrently", "true"),
+					resource.TestCheckResourceAttr(resourceName, "input_option_type", "bigquery"),
+					resource.TestCheckResourceAttr(resourceName, "output_option_type", "snowflake"),
+					resource.TestCheckResourceAttr(resourceName, "input_option.bigquery_input_option.query", "SELECT id, name, email, created_at FROM `test_dataset.test_table`"),
+					resource.TestCheckResourceAttr(resourceName, "input_option.bigquery_input_option.location", "US"),
+					resource.TestCheckResourceAttr(resourceName, "filter_columns.0.name", "user_id"),
+					resource.TestCheckResourceAttr(resourceName, "filter_columns.0.src", "id"),
+					resource.TestCheckResourceAttr(resourceName, "filter_columns.0.type", "long"),
+					resource.TestCheckResourceAttr(resourceName, "output_option.snowflake_output_option.database", "test_database"),
+					resource.TestCheckResourceAttr(resourceName, "output_option.snowflake_output_option.schema", "PUBLIC"),
+					resource.TestCheckResourceAttr(resourceName, "output_option.snowflake_output_option.table", "bigquery_to_snowflake_test_table"),
+					resource.TestCheckResourceAttr(resourceName, "output_option.snowflake_output_option.mode", "insert"),
+				),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{},
+				ImportStateIdFunc: func(s *terraform.State) (string, error) {
+					jobDefinitionId := s.RootModule().Resources[resourceName].Primary.ID
+					return jobDefinitionId, nil
+				},
+			},
+		},
+	})
+}
+
+func TestAccJobDefinitionResourceSnowflakeToBigQuery(t *testing.T) {
+	resourceName := "trocco_job_definition.snowflake_to_bigquery"
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				ResourceName: resourceName,
+				Config:       providerConfig + LoadTextFile("../../examples/testdata/job_definition/snowflake_to_bigquery/create.tf"),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", "Snowflake to BigQuery Test"),
+					resource.TestCheckResourceAttr(resourceName, "description", "Test job definition for transferring data from Snowflake to BigQuery"),
+					resource.TestCheckResourceAttr(resourceName, "resource_enhancement", "medium"),
+					resource.TestCheckResourceAttr(resourceName, "retry_limit", "2"),
+					resource.TestCheckResourceAttr(resourceName, "is_runnable_concurrently", "true"),
+					resource.TestCheckResourceAttr(resourceName, "input_option_type", "snowflake"),
+					resource.TestCheckResourceAttr(resourceName, "output_option_type", "bigquery"),
+					resource.TestCheckResourceAttr(resourceName, "input_option.snowflake_input_option.database", "test_database"),
+					resource.TestCheckResourceAttr(resourceName, "input_option.snowflake_input_option.schema", "PUBLIC"),
+					resource.TestCheckResourceAttr(resourceName, "input_option.snowflake_input_option.query", "SELECT id, name, email, created_at FROM test_table"),
+					resource.TestCheckResourceAttr(resourceName, "filter_columns.0.name", "user_id"),
+					resource.TestCheckResourceAttr(resourceName, "filter_columns.0.src", "id"),
+					resource.TestCheckResourceAttr(resourceName, "filter_columns.0.type", "long"),
+					resource.TestCheckResourceAttr(resourceName, "output_option.bigquery_output_option.dataset", "test_dataset"),
+					resource.TestCheckResourceAttr(resourceName, "output_option.bigquery_output_option.table", "snowflake_to_bigquery_test_table"),
+					resource.TestCheckResourceAttr(resourceName, "output_option.bigquery_output_option.mode", "append"),
+				),
+			},
+			{
+				ResourceName:            resourceName,
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{},
+				ImportStateIdFunc: func(s *terraform.State) (string, error) {
+					jobDefinitionId := s.RootModule().Resources[resourceName].Primary.ID
+					return jobDefinitionId, nil
+				},
+			},
+		},
+	})
+}
