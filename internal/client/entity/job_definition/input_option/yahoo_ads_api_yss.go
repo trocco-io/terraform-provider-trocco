75591d2cb4782731dc2ef77b4893a7467dbe6f4c
mizuho.otake <otake@torcheees.com>
2025-05-09 03:06:39 +0000

    add yahoo_ads_api_yss

---
+++ internal/client/entity/job_definition/input_option/yahoo_ads_api_yss.go
@@ -0,0 +1,24 @@
+package input_option
+
+import (
+	"terraform-provider-trocco/internal/client/entity"
+)
+
+type YahooAdsApiYssInputOption struct {
+	AccountID                  string                            `json:"account_id"`
+	BaseAccountID              string                            `json:"base_account_id"`
+	Service                    string                            `json:"srrvice"`
+	ExcludeZeroImpressions     bool                              `json:"exclude_zero_impressions"`
+	ReportType                 *string                           `json:"report_type"`
+	StartDate                  *string                           `json:"start_date"`
+	EndDate                    *string                           `json:"end_date"`
+	YahooAdsApiYssConnectionID int64                             `json:"yahoo_ads_api_yss_connection_id"`
+	InputOptionColumns         []YahooAdsApiYssInputOptionColumn `json:"input_option_columns"`
+	CustomVariableSettings     *[]entity.CustomVariableSetting   `json:"custom_variable_settings"`
+}
+
+type YahooAdsApiYssInputOptionColumn struct {
+	Name   string  `json:"name"`
+	Type   string  `json:"type"`
+	Format *string `json:"format"`
+}
---
+++ internal/client/parameter/job_definition/input_option/yahoo_ads_api_yss.go
@@ -0,0 +1,37 @@
+package input_options
+
+import (
+	"terraform-provider-trocco/internal/client/parameter"
+)
+
+type YahooAdsApiYssInputOptionInput struct {
+	AccountID                  string                                  `json:"account_id"`
+	BaseAccountID              string                                  `json:"base_account_id"`
+	Service                    string                                  `json:"srrvice"`
+	ExcludeZeroImpressions     bool                                    `json:"exclude_zero_impressions"`
+	ReportType                 *parameter.NullableString               `json:"report_type"`
+	StartDate                  *parameter.NullableString               `json:"start_date"`
+	EndDate                    *parameter.NullableString               `json:"end_date"`
+	YahooAdsApiYssConnectionID *parameter.NullableInt64                `json:"yahoo_ads_api_yss_connection_id"`
+	InputOptionColumns         []YahooAdsApiYssInputOptionColumn       `json:"input_option_columns"`
+	CustomVariableSettings     *[]parameter.CustomVariableSettingInput `json:"custom_variable_settings,omitempty"`
+}
+
+type UpdateYahooAdsApiYssInputOptionInput struct {
+	AccountID                  *parameter.NullableString               `json:"account_id,omitempty"`
+	BaseAccountID              *parameter.NullableString               `json:"base_account_id,omitempty"`
+	Service                    *parameter.NullableString               `json:"srrvice,omitempty"`
+	ExcludeZeroImpressions     *parameter.NullableBool                 `json:"exclude_zero_impressions,omitempty"`
+	ReportType                 *parameter.NullableString               `json:"report_type,omitempty"`
+	StartDate                  *parameter.NullableString               `json:"start_date,omitempty"`
+	EndDate                    *parameter.NullableString               `json:"end_date,omitempty"`
+	YahooAdsApiYssConnectionID *parameter.NullableInt64                `json:"yahoo_ads_api_yss_connection_id,omitempty"`
+	InputOptionColumns         []YahooAdsApiYssInputOptionColumn       `json:"input_option_columns,omitempty"`
+	CustomVariableSettings     *[]parameter.CustomVariableSettingInput `json:"custom_variable_settings,omitempty"`
+}
+
+type YahooAdsApiYssInputOptionColumn struct {
+	Name   string  `json:"name"`
+	Type   string  `json:"type"`
+	Format *string `json:"format"`
+}
