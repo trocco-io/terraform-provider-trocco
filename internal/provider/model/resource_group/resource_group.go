a8f417e05e123ed08888deb91d2d9f37f5824340
Otake <78843951+Otake0114@users.noreply.github.com>
2025-06-02 17:13:34 +0900

    fix type (#115)

--- internal/provider/model/resource_group/resource_group.go
+++ internal/provider/model/resource_group/resource_group.go
@@ -1,17 +1,25 @@
 package resource_group
 
 import (
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
 type ResourceGroupResourceModel struct {
-	ID          types.Int64             `tfsdk:"id"`
-	Name        types.String            `tfsdk:"name"`
-	Description types.String            `tfsdk:"description"`
-	Teams       []TeamRoleResourceModel `tfsdk:"teams"`
+	ID          types.Int64  `tfsdk:"id"`
+	Name        types.String `tfsdk:"name"`
+	Description types.String `tfsdk:"description"`
+	Teams       types.Set    `tfsdk:"teams"`
 }
 
 type TeamRoleResourceModel struct {
 	TeamID types.Int64  `tfsdk:"team_id"`
 	Role   types.String `tfsdk:"role"`
+}
+
+func (TeamRoleResourceModel) AttrTypes() map[string]attr.Type {
+	return map[string]attr.Type{
+		"team_id": types.Int64Type,
+		"role":    types.StringType,
+	}
 }
