e4e658df862d38899e681e4c77c74379c638dc31
okeyaki <shinya.otsuki.4d@gmail.com>
2025-06-24 16:16:00 +0900

    fix types

--- internal/provider/model/pipeline_definition/schedule.go
+++ internal/provider/model/pipeline_definition/schedule.go
@@ -15,10 +15,10 @@ import (
 type Schedule struct {
 	Frequency types.String `tfsdk:"frequency"`
 	TimeZone  types.String `tfsdk:"time_zone"`
-	Day       types.Int32  `tfsdk:"day"`
-	DayOfWeek types.Int32  `tfsdk:"day_of_week"`
-	Hour      types.Int32  `tfsdk:"hour"`
-	Minute    types.Int32  `tfsdk:"minute"`
+	Day       types.Int64  `tfsdk:"day"`
+	DayOfWeek types.Int64  `tfsdk:"day_of_week"`
+	Hour      types.Int64  `tfsdk:"hour"`
+	Minute    types.Int64  `tfsdk:"minute"`
 }
 
 func NewSchedules(ens []*we.Schedule, previous *PipelineDefinition) []*Schedule {
@@ -43,28 +43,21 @@ func NewSchedule(en *we.Schedule) *Schedule {
 	return &Schedule{
 		Frequency: types.StringValue(en.Frequency),
 		TimeZone:  types.StringValue(en.TimeZone),
-		Minute:    types.Int32Value(int32(en.Minute)),
-		Day:       types.Int32PointerValue(int32PtrFromInt64Ptr(en.Day)),
-		DayOfWeek: types.Int32PointerValue(int32PtrFromInt64Ptr(en.DayOfWeek)),
-		Hour:      types.Int32PointerValue(int32PtrFromInt64Ptr(en.Hour)),
+		Minute:    types.Int64Value(en.Minute),
+		Day:       types.Int64PointerValue(en.Day),
+		DayOfWeek: types.Int64PointerValue(en.DayOfWeek),
+		Hour:      types.Int64PointerValue(en.Hour),
 	}
 }
 
-func int32PtrFromInt64Ptr(i64ptr *int64) *int32 {
-	if i64ptr == nil {
-		return nil
-	}
-	i32 := int32(*i64ptr)
-	return &i32
-}
 
 func (m *Schedule) ToInput() *wp.Schedule {
 	return &wp.Schedule{
 		Frequency: m.Frequency.ValueString(),
 		TimeZone:  m.TimeZone.ValueString(),
-		Minute:    int64(m.Minute.ValueInt32()),
-		Day:       &p.NullableInt64{Valid: !m.Day.IsNull(), Value: int64(m.Day.ValueInt32())},
-		DayOfWeek: &p.NullableInt64{Valid: !m.DayOfWeek.IsNull(), Value: int64(m.DayOfWeek.ValueInt32())},
-		Hour:      &p.NullableInt64{Valid: !m.Hour.IsNull(), Value: int64(m.Hour.ValueInt32())},
+		Minute:    m.Minute.ValueInt64(),
+		Day:       &p.NullableInt64{Valid: !m.Day.IsNull(), Value: m.Day.ValueInt64()},
+		DayOfWeek: &p.NullableInt64{Valid: !m.DayOfWeek.IsNull(), Value: m.DayOfWeek.ValueInt64()},
+		Hour:      &p.NullableInt64{Valid: !m.Hour.IsNull(), Value: m.Hour.ValueInt64()},
 	}
 }
