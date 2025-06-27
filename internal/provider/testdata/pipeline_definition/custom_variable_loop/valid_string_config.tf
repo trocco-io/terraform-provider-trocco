2559631ef562bd8ece85db538dc93316180f56b3
Otake <78843951+Otake0114@users.noreply.github.com>
2025-06-27 13:40:52 +0900

    Fix custom variable loop field (#148)
    * add validation
    
    * fix
    
    * add test
    
    * add test

---
+++ internal/provider/testdata/pipeline_definition/custom_variable_loop/valid_string_config.tf
@@ -0,0 +1,79 @@
+resource "trocco_connection" "my_conn" {
+  connection_type = "bigquery"
+
+  name        = "BigQuery Example"
+  description = "This is a BigQuery connection example"
+
+  project_id               = "example"
+  service_account_json_key = <<JSON
+  {
+    "type": "service_account",
+    "project_id": "example-project-id",
+    "private_key_id": "example-private-key-id",
+    "private_key":"-----BEGIN PRIVATE KEY-----\n..."
+  }
+  JSON
+}
+
+resource "trocco_bigquery_datamart_definition" "test_bigquery_datamart_notifications" {
+  name                     = "test_bigquery_datamart_notifications"
+  is_runnable_concurrently = false
+  bigquery_connection_id   = trocco_connection.my_conn.id
+  query                    = "SELECT * FROM examples"
+  query_mode               = "insert"
+  destination_dataset      = "dist_datasets"
+  destination_table        = "dist_tables"
+  write_disposition        = "append"
+
+  custom_variable_settings = [
+    {
+      name  = "$foo$"
+      type  = "string"
+      value = "x"
+    },
+    {
+      name  = "$bar$"
+      type  = "string"
+      value = "y"
+    },
+    {
+      name      = "$date$"
+      type      = "timestamp"
+      quantity  = 0
+      unit      = "date"
+      direction = "ago"
+      format    = "%Y-%m-%d"
+      time_zone = "Asia/Tokyo"
+    }
+  ]
+}
+
+resource "trocco_pipeline_definition" "trocco_bigquery_datamart" {
+  name = "trocco_bigquery_datamart"
+
+  tasks = [
+    {
+      key  = "trocco_bigquery_datamart"
+      type = "trocco_bigquery_datamart"
+
+      trocco_bigquery_datamart_config = {
+        definition_id = trocco_bigquery_datamart_definition.test_bigquery_datamart_notifications.id
+        custom_variable_loop = {
+          type = "string"
+          string_config = {
+            variables = [
+              {
+                name   = "$foo$"
+                values = ["a", "b"]
+              },
+              {
+                name   = "$bar$"
+                values = ["", "c"]
+              }
+            ]
+          }
+        }
+      }
+    }
+  ]
+}
