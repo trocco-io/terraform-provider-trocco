5014cecdb247518eba09ec7f7c479cdd3771bdd9
ThuTrangNtt047 <trang.nguyen@border-z.co.jp>
2026-04-14 11:55:03 +0700

    feat: update  Bigquery connection (#225)
    * implement
    
    * update code
    
    * fix lint
    
    ---------
    
    Co-authored-by: Hiroshi Sakai <ziguzagu@gmail.com>

--- examples/resources/trocco_connection/bigquery.tf
+++ examples/resources/trocco_connection/bigquery.tf
@@ -14,3 +14,27 @@ resource "trocco_connection" "bigquery" {
   }
   JSON
 }
+
+
+resource "trocco_connection" "bigquery_wif" {
+  connection_type = "bigquery"
+  name            = "BigQuery with WIF"
+  description     = "BigQuery connection using Workload Identity Federation authentication"
+
+  # WIF authentication configuration
+  project_id                          = "example"
+  is_workload_identity_federation     = true
+  workload_identity_federation_config = <<JSON
+{
+  "type": "external_account",
+  "audience": "//iam.googleapis.com/projects/123456789/locations/global/workloadIdentityPools/aws-pool/providers/aws-provider",
+  "service_account_impersonation_url": "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/sa-bigquery@my-gcp-project.iam.gserviceaccount.com:generateAccessToken",
+  "subject_token_type": "urn:ietf:params:aws:token-type:aws4_request",
+  "token_url": "https://sts.googleapis.com/v1/token",
+  "credential_source": {
+    "environment_id": "aws1",
+    "regional_cred_verification_url": "https://sts.ap-northeast-1.amazonaws.com?Action=GetCallerIdentity&Version=2011-06-15"
+  }
+}
+JSON
+}
