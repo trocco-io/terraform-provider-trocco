8d01e6ef87700dd1572089bdc6effa74e86dc7b3
ziguzagu <ziguzagu@gmail.com>
2026-04-28 15:33:57 +0900

    Run terraform fmt

--- internal/provider/testdata/job_definition/pagerduty_to_bigquery/create.tf
+++ internal/provider/testdata/job_definition/pagerduty_to_bigquery/create.tf
@@ -1,7 +1,7 @@
 resource "trocco_connection" "bigquery" {
-  connection_type          = "bigquery"
-  name                     = "BigQuery Example"
-  project_id               = "example"
+  connection_type = "bigquery"
+  name            = "BigQuery Example"
+  project_id      = "example"
   service_account_json_key = jsonencode({
     type       = "service_account"
     project_id = "example"
@@ -11,7 +11,7 @@ resource "trocco_connection" "bigquery" {
 resource "trocco_connection" "pagerduty_test" {
   connection_type = "pagerduty"
   name            = "Test Pagerduty Connection"
-  api_key = "test-api_key"
+  api_key         = "test-api_key"
 }
 
 resource "trocco_job_definition" "pagerduty_to_bigquery" {
@@ -28,7 +28,7 @@ resource "trocco_job_definition" "pagerduty_to_bigquery" {
   input_option = {
     pagerduty_input_option = {
       pagerduty_connection_id = trocco_connection.pagerduty_test.id
-      path                   = "escalation_policies"
+      path                    = "escalation_policies"
     }
   }
 
