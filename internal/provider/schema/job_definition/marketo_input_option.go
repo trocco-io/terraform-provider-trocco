ea23f52219846a0d0492a7c56be89477d1e9ab55
ziguzagu <ziguzagu@gmail.com>
2026-05-01 18:06:23 +0900

    Remove unused imports

--- internal/provider/schema/job_definition/marketo_input_option.go
+++ internal/provider/schema/job_definition/marketo_input_option.go
@@ -4,9 +4,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
-	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
